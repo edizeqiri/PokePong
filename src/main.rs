e3ef5ed3c92d1be21f1aabb195b13f1414ea3659
pixelvip <pixelmannvip@gmail.com>
2023-11-15 23:03:46 +0100

    add ball collision system

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use bevy::a11y::accesskit::Role::Math;
 use bevy::prelude::*;
 
 fn main() {
@@ -11,6 +10,7 @@ fn main() {
         ))
         .add_systems(Startup, setup)
         .add_systems(Update, ball_movement)
+        .add_systems(Update, ball_collision_system)
         .add_systems(Update, sprite_movement)
         .run();
 }
@@ -24,48 +24,54 @@ enum Direction {
 #[derive(Component)]
 enum Player {
     One,
-    Two
+    Two,
 }
 
 #[derive(Component)]
-struct Ball;
+struct Ball {
+    speed: f32,
+    direction: Vec3,
+}
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
     commands.spawn((
         SpriteBundle {
             texture: asset_server.load("karpador.png"),
-            transform: Transform::from_xyz(500., 0., 0.).with_scale(Vec3::new(0.25,0.25,0.25)),
+            transform: Transform::from_xyz(500., 0., 0.).with_scale(Vec3::new(0.25, 0.25, 0.25)),
             ..default()
         },
         Player::One,
-        Direction::Up
+        Direction::Up,
     ));
     commands.spawn((
         SpriteBundle {
             texture: asset_server.load("karpador.png"),
-            transform: Transform::from_xyz(0., 0., 0.).with_scale(Vec3::new(0.25,0.25,0.25)),
+            transform: Transform::from_xyz(0., 0., 0.).with_scale(Vec3::new(0.25, 0.25, 0.25)),
             ..default()
         },
-        Ball
+        Ball {
+            speed: 100.,
+            direction: Vec3::new(1., 0., 0.).normalize(),
+        },
     ));
     commands.spawn((
         SpriteBundle {
             texture: asset_server.load("karpador.png"),
-            transform: Transform::from_xyz(-500., 0., 0.).with_scale(Vec3::new(0.25,0.25,0.25)),
+            transform: Transform::from_xyz(-500., 0., 0.).with_scale(Vec3::new(0.25, 0.25, 0.25)),
             ..default()
         },
         Player::Two,
-        Direction::Down
+        Direction::Down,
     ));
 }
 
 fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
     for (mut logo, mut transform) in &mut sprite_position {
-       match *logo {
-           Direction::Up => transform.translation.y += 150. * time.delta_seconds(),
-           Direction::Down => transform.translation.y -= 150. * time.delta_seconds(),
-       }
+        match *logo {
+            Direction::Up => transform.translation.y += 150. * time.delta_seconds(),
+            Direction::Down => transform.translation.y -= 150. * time.delta_seconds(),
+        }
 
         if transform.translation.y > 100. {
             *logo = Direction::Down;
@@ -75,15 +81,30 @@ fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, 
     }
 }
 
-fn ball_movement(time: Res<Time>, mut balls: Query<(&mut Ball, &mut Transform)>, mut players: Query<(&mut Player, &mut Transform)>) {
+fn ball_movement(time: Res<Time>, mut ball_query: Query<(&Ball, &mut Transform)>) {
+    for (ball, mut transform) in ball_query.iter_mut() {
+        transform.translation += ball.direction * ball.speed * time.delta_seconds();
+    }
+}
 
-    for (mut ball, mut ball_transform) in &mut balls {
-        for (mut player, mut player_transform) in &mut players {
-            if ball_transform.translation.x >= player_transform.translation.x {
-
+fn ball_collision_system(
+    mut ball_query: Query<(&mut Ball, &Transform)>,
+    player_query: Query<(&Player, &Transform)>,
+) {
+    for (mut ball, ball_transform) in ball_query.iter_mut() {
+        for (player, player_transform) in &player_query {
+            match player {
+                Player::One => {
+                    if player_transform.translation.x <= ball_transform.translation.x {
+                        ball.direction.x *= -1.
+                    }
+                }
+                Player::Two => {
+                    if player_transform.translation.x >= ball_transform.translation.x {
+                        ball.direction.x *= -1.
+                    }
+                }
             }
         }
-
-
     }
 }
