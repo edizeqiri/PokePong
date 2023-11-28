99d52d74b1109599a60cbd3c8248296004765817
pixelvip <pixelmannvip@gmail.com>
2023-11-28 23:54:55 +0100

    add player movement and make it pretty

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
+        .insert_resource(ClearColor(Color::rgb(0.984, 0.929, 0.761)))
         .add_plugins(DefaultPlugins.set(
             // This sets image filtering to nearest
             // This is done to prevent textures with low resolution (e.g. pixel art) from being blurred
@@ -11,6 +12,7 @@ fn main() {
         .add_systems(Startup, setup)
         .add_systems(Update, ball_movement)
         .add_systems(Update, ball_collision_system)
+        .add_systems(Update, player_movement)
         .add_systems(Update, sprite_movement)
         .run();
 }
@@ -21,10 +23,15 @@ enum Direction {
     Down,
 }
 
-#[derive(Component)]
-enum Player {
+enum Paddle {
     One,
     Two,
+}
+
+#[derive(Component)]
+struct Player {
+    paddle: Paddle,
+    speed: f32,
 }
 
 #[derive(Component)]
@@ -37,17 +44,19 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
     commands.spawn((
         SpriteBundle {
-            texture: asset_server.load("karpador.png"),
-            transform: Transform::from_xyz(500., 0., 0.).with_scale(Vec3::new(0.25, 0.25, 0.25)),
+            texture: asset_server.load("pokepaddle.png"),
+            transform: Transform::from_xyz(-500., 0., 0.),
             ..default()
         },
-        Player::One,
-        Direction::Up,
+        Player {
+            paddle: Paddle::One,
+            speed: 300.,
+        },
     ));
     commands.spawn((
         SpriteBundle {
-            texture: asset_server.load("karpador.png"),
-            transform: Transform::from_xyz(0., 0., 0.).with_scale(Vec3::new(0.25, 0.25, 0.25)),
+            texture: asset_server.load("pokeball.png"),
+            transform: Transform::from_xyz(0., 0., 0.),
             ..default()
         },
         Ball {
@@ -57,12 +66,14 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     ));
     commands.spawn((
         SpriteBundle {
-            texture: asset_server.load("karpador.png"),
-            transform: Transform::from_xyz(-500., 0., 0.).with_scale(Vec3::new(0.25, 0.25, 0.25)),
+            texture: asset_server.load("pokepaddle.png"),
+            transform: Transform::from_xyz(500., 0., 0.),
             ..default()
         },
-        Player::Two,
-        Direction::Down,
+        Player {
+            paddle: Paddle::Two,
+            speed: 300.,
+        },
     ));
 }
 
@@ -80,6 +91,38 @@ fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, 
         }
     }
 }
+fn player_movement(
+    time: Res<Time>,
+    keys: Res<Input<KeyCode>>,
+    mut player_query: Query<(&Player, &mut Transform)>,
+) {
+    for (player, mut transform) in player_query.iter_mut() {
+        match player.paddle {
+            Paddle::One => {
+                let mut direction = 0.;
+
+                if keys.pressed(KeyCode::W) {
+                    direction = 1.;
+                }
+                if keys.pressed(KeyCode::S) {
+                    direction = -1.;
+                }
+                transform.translation.y += player.speed * direction * time.delta_seconds()
+            }
+            Paddle::Two => {
+                let mut direction = 0.;
+
+                if keys.pressed(KeyCode::Up) {
+                    direction = 1.;
+                }
+                if keys.pressed(KeyCode::Down) {
+                    direction = -1.;
+                }
+                transform.translation.y += player.speed * direction * time.delta_seconds()
+            }
+        }
+    }
+}
 
 fn ball_movement(time: Res<Time>, mut ball_query: Query<(&Ball, &mut Transform)>) {
     for (ball, mut transform) in ball_query.iter_mut() {
@@ -93,13 +136,13 @@ fn ball_collision_system(
 ) {
     for (mut ball, ball_transform) in ball_query.iter_mut() {
         for (player, player_transform) in &player_query {
-            match player {
-                Player::One => {
+            match player.paddle {
+                Paddle::One => {
                     if player_transform.translation.x <= ball_transform.translation.x {
                         ball.direction.x *= -1.
                     }
                 }
-                Player::Two => {
+                Paddle::Two => {
                     if player_transform.translation.x >= ball_transform.translation.x {
                         ball.direction.x *= -1.
                     }
